78ed41cd52e004bbfae8c1817270764ad494f97d
Dolfino <tobybanana40@gmail.com>
2025-03-22 13:13:17 +1100

    Added some client/server commands, working on smooth shutdowns

--- newServer.py
+++ newServer.py
@@ -3,32 +3,119 @@ import threading
 import json
 
 
-def handleClient(conn, addr):
+def handleClient(conn, addr, plateManager):
+    global running
     print(f"NEW CONNECTION: {addr} connected")
     
     connected = True
     while connected:
+        if not running:
+            send(conn, DCMSG)
+            break
+        try:
+            msgLength = conn.recv(HEADER)
+            if msgLength:
+                msgLength = int(msgLength)
+                msg = str(conn.recv(msgLength).decode(FORMAT))
 
-        msgLength = conn.recv(HEADER)
-        if msgLength:
-            msgLength = int(msgLength)
-            msg = str(conn.recv(msgLength).decode(FORMAT))
-            
-            if msg == DCMSG:
-                connected = False
-                print(f"{addr} Disconnected.")
-            else:
-                print(f"[{addr}]: {msg}")
+                if msg == DCMSG:
+                    connected = False
+                    print(f"{addr} Disconnected.")
+                else:
+                    print(msg)
+                    handleArgs(conn, msg, plateManager)
+        except ConnectionResetError:
+            connected = False
 
 
-def start():
+def send(conn, msg):
+    if conn == "BANK":
+        print(str(msg))
+    else:
+        print("Sending...")
+        msg = str(msg).encode(FORMAT)
+
+        msgLength = str(len(msg)).encode(FORMAT)
+        print(msgLength)
+        conn.send(msgLength)
+        print("Sent Header")
+        conn.send(msg)
+        print("Sent Message")
+
+def start(plateManager):
+    global running
     server.listen()
 
     while True:
-        conn, addr = server.accept()
-        thread = threading.Thread(target=handleClient, args=(conn, addr))
-        thread.start()
-        print(f"ACTIVE CONNECTIONS: {threading.active_count() - 1}")
+        try:
+            if not running:
+                break
+            conn, addr = server.accept()
+            thread = threading.Thread(target=handleClient, args=(conn, addr, plateManager))
+            thread.start()
+            print(f"ACTIVE CONNECTIONS: {threading.active_count() - 1}")
+        except OSError:
+            break
+
+
+def handleArgs(conn, msg, plateManager):
+    global running
+    args = msg.split(":")
+    sent = False
+
+    if args[0] == "TRYCHARGE" and len(args) >= 3: # TRYCHARGE:[PLATE]:[PIN]:[AMOUNT]
+
+        for user in plateManager.users:
+            if user.plate == args[1] and user.pin == args[2]:
+                if (user.balance - float(args[3]) >= 0):
+                    user.balance -= float(args[3])
+                    plateManager.balance += float(args[3])
+                    #print(
+                    #    f"CHARGED {user.plate} ${args[3]}.\nNEW USER BALANCE: ${user.balance}.\nNEW BANK BALANCE: ${plateManager.balance}")
+                    break
+                else:
+                    send(conn, "NULL")
+
+    elif args[0] == "GETBANKBALANCE": # GETBANKBALANCE
+        send(conn, plateManager.balance)
+
+    elif args[0] == "GETPLATEINFO" and len(args) >= 2: # GETPLATEINFO:[PLATE]
+        for user in plateManager.users:
+            if user.plate == args[1]:
+                send(conn, f"{user.pin}:{user.balance}")
+                sent = True
+                break
+        if not sent:
+            send(conn, "NULL")
+
+    elif args[0] == "REGISTERPLATE" and len(args) >= 4: # TRYREGISTERPLATE:[PLATE]:[PIN]:[BALANCE]
+        plateExists = False
+        for user in plateManager.users:
+            if user.plate == args[1]:
+                send(conn, "NULL")
+                plateExists = True
+                break
+        if not plateExists:
+            newUser = User()
+            newUser.initialiseNewUser(args[1], int(args[2]), float(args[3]))
+            plateManager.users.append(newUser)
+
+    elif args[0] == "SHUTDOWN": # SHUTDOWN:[AUTHCODE]
+        if len(args) == 2:
+            if args[1] == AUTHCODE:
+                plateManager.save()
+                running = False
+                server.close()
+
+
+
+
+def getConsoleInput(plateManager):
+    global running
+    while running:
+        msg = input("")
+        handleArgs("BANK", msg, plateManager)
+
 
 class User:
     def __init__(self):
@@ -38,13 +125,13 @@ class User:
 
     def initialiseNewUser(self, plate, pin, balance):
         self.plate = plate
-        self.pin = pin
-        self.balance = balance
+        self.pin = int(pin)
+        self.balance = float(balance)
 
     def initialiseFromJson(self, plate, info):
         self.plate = plate
-        self.pin = info["pin"]
-        self.balance = info["balance"]
+        self.pin = int(info["pin"])
+        self.balance = float(info["balance"])
 
     def toJSON(self):
 
@@ -61,11 +148,20 @@ class User:
 
 
 class PlateManager:
-    def __init__(self, logFile):
+    def __init__(self, logFile, balance, bankFile):
         self.users = []
         self.logFile = logFile
+        self.bankFile = bankFile
+        self.balance = balance
 
     def load(self):
+
+        fb = open(self.bankFile, "r")
+        try:
+            self.balance = float(fb.read())
+        except:
+            print("No pre-existing balance. Setting balance to 0...")
+            self.balance = 0
         f = open(self.logFile, "r")
 
         try:
@@ -83,6 +179,8 @@ class PlateManager:
         f.close()
 
     def save(self):
+        fb = open(self.bankFile, "w")
+        fb.write(str(self.balance))
         f = open(self.logFile, "w")
         userDict = {}
         for user in self.users:
@@ -95,23 +193,10 @@ class PlateManager:
 
     def __str__(self):
 
-        returnStr = ""
+        returnStr = f"BANK OF MICAH:\nBALANCE: {self.balance}\n"
         for user in self.users:
             returnStr = returnStr + f"{user.plate} || {user.pin} || {user.balance}\n"
         return returnStr
-
-
-
-
-plateManager = PlateManager("data.json")
-plateManager.load()
-
-tobyOakes = User()
-tobyOakes.initialiseNewUser("Y2390W", "3561", "9135")
-plateManager.addUser(tobyOakes)
-print(plateManager)
-plateManager.save()
-
 
 
 HEADER = 64
@@ -119,12 +204,21 @@ FORMAT = "utf-8"
 PORT = 5050
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
-DCMSG = "--DISCONNECT"
+DCMSG = "DISCONNECT"
+AUTHCODE = "ELMO"
+running = True
 print(SERVER)
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDR)
 
+plateManager = PlateManager("data.json", 0.0, "bankData.txt")
+plateManager.load()
+print(plateManager)
 
 print("STARTING...")
-start()
+handleConsole = threading.Thread(target=getConsoleInput,args=[plateManager])
+handleConsole.start()
+
+start(plateManager)
+
